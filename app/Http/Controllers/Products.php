a470720b79e88fa05efca1c088fce61e30cdaa5d
azizinari2018 <52975685+azizinari2018@users.noreply.github.com>
2019-07-22 10:58:34 +0400

    all not deleted products

--- app/Http/Controllers/Products.php
+++ app/Http/Controllers/Products.php
@@ -11,7 +11,7 @@ use Darryldecode\Cart\Facades\CartFacade as Cart;
 class Products extends Controller
 {
     function index(){
-    	$products = Product::all();
+    	$products = Product::where('is_deleted','0')->get();
     	$data = [
 		    'products'  => $products,
 		    'cartCount'   => count(Cart::getContent()),
